2c17cd57d7b3724de14e49a81b0cbf9b5cb1b5f9
skywalker512 <houzhenhong@hotmail.com>
2018-11-29 13:37:22 +0800

    留言板 添加js

---
+++ redrock4/level2/assets/main.js
@@ -0,0 +1,96 @@
+function initMain() {
+    data.map(createCommentElement)
+    
+    var postElement = document.querySelector("#post");
+    postElement.addEventListener("click", postClickEvent);
+
+    var bodyElmemet = document.querySelector("body")
+    bodyElmemet.addEventListener("click", showTextArae)
+
+    var submitElment = document.querySelector("#submit")
+    submitElment.addEventListener("click", pushText)
+}
+
+function createCommentElement(currentValue){
+    var commentElement = document.querySelector("#comment");
+    commentElement.insertAdjacentHTML("beforeend", '<div class="articles">'
+    +'<div class="avatar"><img src="https://cdn.v2ex.com/gravatar/972a36b803d3da31e70252ccfe56e9ce?d=retro&amp;s=64" alt="" class="avatar"></div>'
+    +'<div class="article">'
+        +'<div class="articlecontent">'
+            +'<div class="info">'
+                +'<div class="name">'+ currentValue.name +'</div>'
+                +'<div class="data">'+ currentValue.dete +'</div>'
+                +'<div class="ua">'+ currentValue.ua +'</div>'
+            +'</div>'
+            +'<div class="content">'+ currentValue.content +'</div>'
+        +'</div>'
+    +'</div>'
+    +'</div>');        
+}
+
+
+function postClickEvent (event) {
+    event.stopPropagation()
+    var showElement = document.querySelector("#show")
+    showElement.style.cssText = "display: block"
+    var articleContentElement = document.querySelector(".articles.comment.postcomment .articlecontent")
+    articleContentElement.style.cssText = "height: 260px !important;"
+}
+
+function showTextArae() {
+    var showElement = document.querySelector("#show")
+    showElement.style.cssText = "display: none;"
+    var articleContentElement = document.querySelector(".articles.comment.postcomment .articlecontent")
+    articleContentElement.style.cssText = "height: 44px !important;"
+}
+
+function pushText() {
+    var msg = document.getElementById('content').value
+    if (msg.length>1) {
+        data[data.length] = {name:"houzhen", dete:getTime(), ua:"chrome", content:msg,}
+        createCommentElement(data[data.length-1])
+        document.getElementById('content').value = ""
+    }
+}
+function getTime() {
+    var date = new Date();
+
+    // 获取当前月份
+    var nowMonth = date.getMonth() + 1;
+    
+    // 获取当前是几号
+    var strDate = date.getDate();
+    
+    // 添加分隔符“-”
+    var seperator = "-";
+    
+    // 对月份进行处理，1-9月在前面添加一个“0”
+    if (nowMonth >= 1 && nowMonth <= 9) {
+       nowMonth = "0" + nowMonth;
+    }
+    
+    // 对月份进行处理，1-9号在前面添加一个“0”
+    if (strDate >= 0 && strDate <= 9) {
+       strDate = "0" + strDate;
+    }
+    
+    // 最后拼接字符串，得到一个格式为(yyyy-MM-dd)的日期
+    var nowDate = date.getFullYear() + seperator + nowMonth + seperator + strDate;
+
+    return nowDate;
+    
+    // 作者：LeoLeeYEAH
+    // 链接：https://www.jianshu.com/p/3415c3aa722d
+    // 來源：简书
+    // 简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
+}
+
+// function getBrowserInfo(){
+//     var Sys = {};
+//     var ua = navigator.appVersion
+//     var re =/(msie|firefox|chrome|opera|version).*?([\d.]+)/;
+//     var m = ua.match(re);
+//     Sys.browser = m[1].replace(/version/, "'safari");
+//     Sys.ver = m[2];
+//     return Sys;
+// }
