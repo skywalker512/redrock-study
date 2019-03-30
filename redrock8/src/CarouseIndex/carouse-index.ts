e3bce3a295994d6a7c0b195404383cfd43b9e66e
skywalker512 <houzhenhong@hotmail.com>
2019-03-30 15:59:27 +0800

    [提交] 将滑动交由 js 控制

--- redrock8/src/CarouseIndex/carouse-index.ts
+++ redrock8/src/CarouseIndex/carouse-index.ts
@@ -5,7 +5,7 @@ import style from './carouse-index.less'
 
 import apiData from '../json/index.json'
 
-import {CarouselItem} from '../Item/item'
+import { CarouselItem } from '../Item/item'
 
 @CustomElement({
   tag: 'carouse-index',
@@ -15,7 +15,7 @@ import {CarouselItem} from '../Item/item'
 })
 export class CarouselIndex extends HTMLElement {
   _list: { "content": string; "img": string; "name": string; }[]
-  $itmes: HTMLUListElement;
+  $slides: HTMLUListElement;
   $inputs: HTMLDivElement;
   _activeNum: number;
   $slidesNavigation: HTMLDivElement;
@@ -26,16 +26,42 @@ export class CarouselIndex extends HTMLElement {
     this._activeNum
   }
   connectedCallback() {
-    this.$itmes = this.shadowRoot.querySelector('ul.slides')
+    // 模版 html 中有的
+    this.$slides = this.shadowRoot.querySelector('ul.slides')
     this.$slidesNavigation = this.shadowRoot.querySelector('div#slidesNavigation')
+
     this.__render()
+
+    // 动态生成的
+    this.$slidesNavigation.addEventListener('click', this.handelRadioClick)
+    this.$slidesNavigation.firstElementChild.classList.add('checked')
+    this._activeNum = 0
   }
   __render() {
-    this._list.forEach((item, index)=>{
+    this._list.forEach((item) => {
+      // 填入信息
       const $item = <CarouselItem>document.createElement('carouse-item')
       $item._item = item
-      this.$itmes.appendChild($item)
-      this.$slidesNavigation.insertAdjacentHTML('beforeend', `<div class="radio"></div>`)
+      this.$slides.appendChild($item)
+
+      // 单选圆点
+      const $radio = document.createElement('div')
+      $radio.classList.add('radio')
+      this.$slidesNavigation.appendChild($radio)
     })
+    // 将div撑开
+    this.$slides.style.width = `${this._list.length * 100}%`
+  }
+
+  handelRadioClick: EventListener = e => {
+    if (e.srcElement.matches('.radio')) {
+      e.srcElement.parentElement.childNodes.forEach((item: HTMLElement, index) => {
+        if (item.classList.contains('checked')) item.classList.remove('checked')
+        // 移动
+        if (item === e.srcElement) this._activeNum = index
+      })
+      e.srcElement.classList.add('checked')
+      this.$slides.style.transform = `translateX(-${(this._activeNum / this._list.length) * 100}%)`
+    }
   }
 }
