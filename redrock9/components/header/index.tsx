1e465fcb7b07a8b376ddadafc19afd6d9569848a
houzhenhong <houzhenhong@hotmail.com>
2019-04-16 23:44:44 +0800

    [提交] 完成头部

--- redrock9/components/header/index.tsx
+++ redrock9/components/header/index.tsx
@@ -1,11 +1,48 @@
 import React from 'react'
-import { Header, Nav } from './styled'
-import Twitter from '@/assets/svg/twitter.svg'
+import { Header, Nav, Item } from './styled'
+
+import UserIcon from '@/assets/svg/user.svg'
+import FileIcon from '@/assets/svg/file-o.svg'
+import FaceBookIcon from '@/assets/svg/facebook.svg'
+import TwitterIcon from '@/assets/svg/twitter.svg'
+import TumblrIcon from '@/assets/svg/tumblr.svg'
+import PinterestIcon from '@/assets/svg/pinterest.svg'
+
 export default () => {
   return (
     <Header>
       <Nav>
-        <Twitter></Twitter>
+        <div>
+          <Item>
+            <UserIcon />
+            <span>Agent Login</span>
+          </Item>
+          <Item>
+            <UserIcon />
+            <span>Customer Login</span>
+          </Item>
+          <Item>
+            <FileIcon />
+            <span>Customer Login<span className='link'>Register?</span></span>
+          </Item>
+          <Item>
+            <span>Call Us Now: 815-123-4567</span>
+          </Item>
+        </div>
+        <div>
+          <Item>
+            <FaceBookIcon />
+          </Item>
+          <Item>
+            <TwitterIcon />
+          </Item>
+          <Item>
+            <TumblrIcon />
+          </Item>
+          <Item>
+            <PinterestIcon />
+          </Item>
+        </div>
       </Nav>
     </Header>
   )
--- redrock9/components/header/styled.ts
+++ redrock9/components/header/styled.ts
@@ -3,9 +3,44 @@ import styled from "styled-components"
 export const Header = styled.header`
   height: 2.5rem;
   background-color: #2c3e50;
+  min-width: 75rem;
 `
 export const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
   height: 100%;
-  max-width: 75rem;
+  font-size: 0.75rem;
+  width: 75rem;
+  line-height: 2.5rem;
   margin: auto;
+  color: #fff;
+  svg {
+    margin-right: 0.5rem;
+  }
+  div {
+    display: flex;
+  }
+  div:last-child {
+    a {
+      padding: 0 0.75rem;
+    }
+    a:first-child {
+      border-left: 0.04rem solid #17202a;
+    }
+    svg {
+      margin: 0;
+    }
+  }
+`
+
+export const Item = styled.a`
+  display: flex;
+  align-items: center;
+  padding: 0 1.25rem;
+  border-right: 0.04rem solid #17202a;
+  cursor: pointer;
+  .link {
+    padding-left: 0.3rem;
+    color: #1dd2af;
+  }
 `
