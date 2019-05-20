c5b37d9c5fbf287a38c23f92fec48d400628c01d
houzhenhong <houzhenhong@hotmail.com>
2019-05-20 23:35:26 +0800

    style(index): 代码风格
    1. 去掉不需要的 interface
    2. 括弧之间的空格
    3. 三元运算符号换为 if else
    4. 去掉注释

--- redrock8/src/CustomElement/index.ts
+++ redrock8/src/CustomElement/index.ts
@@ -7,45 +7,42 @@ export interface CustomElementMetadata {
   style?: string
   shadow: boolean
 }
+export const CustomElement = (args: CustomElementMetadata) => {
+  return (target: any) => {
+    // 在解构的时候不需要定义 类型
+    const {tag} = args;
 
-export interface KeyValue {
-  [key: string]: any
-}
+    // 装饰器不能写在没有命名的类中
+    class CustomElement extends target {
+      showShadowRoot: boolean;
 
-export const CustomElement = (args: CustomElementMetadata) => (target: any) => {
-  // 在解构的时候不需要定义 类型
-  const { tag } = args
-  // 装饰器不能写在没有命名的类中
-  class CustomElement extends target {
-    showShadowRoot: boolean
-    constructor() {
-      super()
-      this.showShadowRoot = args.shadow
-      if (!this.shadowRoot && this.showShadowRoot) {
-        this.attachShadow({ mode: 'open' })
+      constructor() {
+        super();
+        this.showShadowRoot = args.shadow;
+        if (!this.shadowRoot && this.showShadowRoot) {
+          this.attachShadow({mode: 'open'})
+        }
+      }
+
+      connectedCallback() {
+        this.___render();
+        super.connectedCallback && super.connectedCallback();
+        addEventListeners(this)
+      }
+
+      ___render() {
+        const template = document.createElement('template');
+        const style = `${args.style ? `<style>${args.style}</style>` : ''}`;
+        template.innerHTML = `${style}${args.template ? args.template : ''}`;
+
+        if (this.showShadowRoot) {
+          this.shadowRoot.appendChild(template.content)
+        } else {
+          this.appendChild(template.content)
+        }
       }
     }
-    connectedCallback() {
-      this.___render()
-      super.connectedCallback && super.connectedCallback()
-      addEventListeners(this)
-    }
-    ___render() {
-      const template = document.createElement('template')
-      const style = `${args.style ? `<style>${args.style}</style>` : ''}`
-      template.innerHTML = `${style}${args.template ? args.template : ''}`; // 这里为什么必须要 ; 
-      (this.showShadowRoot ? this.shadowRoot : this).appendChild(template.content)
-    }
-  }
-  customElements.define(tag, CustomElement)
-}
 
-// const connectSuper = (target: object, property: string, descriptor: any) => {
-//   const oldValue = descriptor.value
-//   descriptor.value = function() {
-//     oldValue.apply(this, arguments)
-//     Object.getPrototypeOf(target)[property] && Object.getPrototypeOf(target)[property].apply(this)
-//   }
-//   // 传回去的 descriptor 会在 类中执行，即会绑定相应的 this
-//   return descriptor
-// }
+    customElements.define(tag, CustomElement)
+  };
+};
