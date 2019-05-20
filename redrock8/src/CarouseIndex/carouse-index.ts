92291b2ed6759ee848386fb9242ca283727751d7
houzhenhong <houzhenhong@hotmail.com>
2019-05-20 23:45:26 +0800

    style(carouse): 代码风格
    1. ts 规定返回类型
    2. ts 类型断言
    3. 拉平 if else
    4. 拼写

--- redrock8/src/CarouseIndex/carouse-index.ts
+++ redrock8/src/CarouseIndex/carouse-index.ts
@@ -16,52 +16,53 @@ import { CarouselItem } from '../Item/item'
 })
 export class CarouselIndex extends HTMLElement {
   $slides: HTMLUListElement;
-  $inputs: HTMLDivElement;
   $slidesNavigation: HTMLDivElement;
-  $carrousel: HTMLDivElement;
+  $carousel: HTMLDivElement;
   state: {
     list: { content: string; img: string; name: string; }[],
     activeNum: number,
     time: number,
     autoChangeNum: number,
     transitionMode: string,
-  }
+  };
 
   constructor() {
-    super()
+    super();
     this.state = { list: [], activeNum: null, time: null, autoChangeNum: null, transitionMode: null }
   }
-  connectedCallback() {
+
+  connectedCallback(): void {
     // 注入 state
-    this.state.list = apiData
-    this.state.activeNum = 0
-    this.state.time = parseInt(this.getAttribute('time'))
-    this.state.transitionMode = this.getAttribute('transition-mode')
+    this.state.list = apiData;
+    this.state.activeNum = 0;
+    this.state.time = parseInt(this.getAttribute('time'));
+    this.state.transitionMode = this.getAttribute('transition-mode');
 
     // 模版 html 中有的
-    this.$slides = this.shadowRoot.querySelector('ul.slides')
-    this.$slidesNavigation = this.shadowRoot.querySelector('div#slidesNavigation')
-    this.$carrousel = this.shadowRoot.querySelector('div.carrousel')
+    this.$slides = this.shadowRoot.querySelector('ul.slides');
+    this.$slidesNavigation = this.shadowRoot.querySelector('div#slidesNavigation');
+    this.$carousel = this.shadowRoot.querySelector('div.carousel');
 
-    this.__render()
+    this.__render();
 
     // 动态生成的
-    this.$slidesNavigation.firstElementChild.classList.add('checked')
+    this.$slidesNavigation.firstElementChild.classList.add('checked');
 
     this.handelAutoChange()
   }
+
   __render() {
     this.state.list.forEach((item) => {
       // 填入信息
-      const $item = <CarouselItem>document.createElement('carouse-item')
-      $item._item = item
-      this.$slides.appendChild($item)
+      const $item = <CarouselItem>document.createElement('carouse-item');
+      $item._item = item;
+      this.$slides.appendChild($item);
 
       // 单选圆点
-      const $radio = document.createElement('div')
-      $radio.classList.add('radio')
+      const $radio = document.createElement('div');
+      $radio.classList.add('radio');
       this.$slidesNavigation.appendChild($radio)
-    })
+    });
     // 将div撑开
     if(this.state.transitionMode==='slide') this.$slides.style.width = `${this.state.list.length * 100}%`
   }
@@ -69,35 +70,42 @@ export class CarouselIndex extends HTMLElement {
   // 通过装饰器后 使用 call 绑定了 this, 不再需要使用 箭头方法了
   @Listen('click', '$slidesNavigation')
   handelRadioClick (e:Event) {
-    if (e.srcElement.matches('.radio')) {
-      e.srcElement.parentNode.childNodes.forEach((item: HTMLElement, index: number)=>{
-        if (item === e.srcElement) this.handelChangeRadio(index)
-      })
+    const ele = <HTMLElement>e.srcElement;
+    if (!ele.matches('.radio')) {
+      return;
     }
+    ele.parentNode.childNodes.forEach((item: HTMLElement, index: number) => {
+      if (item === ele) this.handelChangeRadio(index)
+    })
   }
+
   // mouseenter 不会冒泡，到了才触发
-  @Listen('mouseenter', '$carrousel')
+  @Listen('mouseenter', '$carousel')
   handelMouseenter () {
     clearInterval(this.state.autoChangeNum)
   }
-  @Listen('mouseleave', '$carrousel')
+
+  @Listen('mouseleave', '$carousel')
   handelMouseleave () {
     this.handelAutoChange()
   }
 
   handelChangeRadio = (index:number) => {
     this.$slidesNavigation.childNodes.forEach((item: HTMLElement, num) => {
-      if (num === this.state.activeNum) item.classList.remove('checked')
+      if (num === this.state.activeNum) item.classList.remove('checked');
       if (num === index) item.classList.add('checked')
-    })
-    this.state.activeNum = index
-    if(this.state.transitionMode==='slide') this.$slides.style.transform = `translateX(-${(this.state.activeNum / this.state.list.length) * 100}%)`
-  }
+    });
+    this.state.activeNum = index;
+    if(this.state.transitionMode==='slide') {
+      this.$slides.style.transform = `translateX(-${(this.state.activeNum / this.state.list.length) * 100}%)`
+    }
+  };
+
   handelAutoChange = () => {
-    if (this.state.time !== NaN) {
+    if (!isNaN(this.state.time)) {
       this.state.autoChangeNum = setInterval(this.handelAutoChangeRadio, this.state.time)
     }
-  }
+  };
 
   handelAutoChangeRadio = () => {
     this.handelChangeRadio((this.state.activeNum+1)%this.state.list.length)
